591340eac5880c8d37ab3850501569c14f2afb05
alturist-su <aimsushmitasingh@gmail.com>
2022-11-11 23:04:58 +0530

    "issue resolved"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import LinearStepper from "./LinearStepper";
 import { Container, Paper, Box } from "@material-ui/core";
-import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
-        <Navbar/>
         <Container component={Box} p={4}>
         <Paper component={Box} p={3}>
           <LinearStepper />
